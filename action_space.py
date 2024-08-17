367ca644ceae4f64610d4296d0cfe6ae2d8bb0da
Benjamin P. Jones <benjaminpaikjones@gmail.com>
2024-08-16 23:17:44 -0400

    Change lists to tuples in action maps

--- action_space.py
+++ action_space.py
@@ -1,15 +1,15 @@
 def make_action_map(size: int):
-    m = { (size * x + y): [x + 1, y + 1] for y in range(size) for x in range(size) }
+    m = { (size * x + y): (x + 1, y + 1) for y in range(size) for x in range(size) }
     m[len(m)] = 'pass'
     m[len(m)] = 'resign'
     return m
 
 action_map_5x5 = make_action_map(5)
-reverse_action_map_5x5 = {tuple(v): k for k, v in action_map_5x5.items()}
+reverse_action_map_5x5 = {v: k for k, v in action_map_5x5.items()}
 
 action_map_9x9 = make_action_map(9)
-reverse_action_map_9x9 = {tuple(v): k for k, v in action_map_9x9.items()}
+reverse_action_map_9x9 = {v: k for k, v in action_map_9x9.items()}
 
 
 action_map_19x19 = make_action_map(19)
-reverse_action_map_19x19 = {tuple(v): k for k, v in action_map_19x19.items()}
+reverse_action_map_19x19 = {v: k for k, v in action_map_19x19.items()}
